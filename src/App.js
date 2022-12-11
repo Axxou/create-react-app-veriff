b5adfce68b7bd79a6c1bcc608e01c06e3a15d928
Axxou <32345343+Axxou@users.noreply.github.com>
2022-12-11 03:00:53 +0100

    Entrée

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class LambdaDemo extends Component {
   constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
+    super(props);
+    this.state = { loading: false, msg: null };
   }
 
-  handleClick = api => e => {
-    e.preventDefault()
+  handleClick = (api) => (e) => {
+    e.preventDefault();
 
-    this.setState({ loading: true })
+    this.setState({ loading: true });
     fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
+      .then((response) => response.json())
+      .then((json) => this.setState({ loading: false, msg: json.msg }));
+  };
 
   render() {
-    const { loading, msg } = this.state
+    const { loading, msg } = this.state;
 
     return (
       <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
+        <button onClick={this.handleClick("hello")}>
+          {loading ? "Loading..." : "Call Lambda"}
+        </button>
+        <button onClick={this.handleClick("async-dadjoke")}>
+          {loading ? "Loading..." : "Call Async Lambda"}
+        </button>
         <br />
         <span>{msg}</span>
       </p>
-    )
+    );
   }
 }
 
@@ -43,9 +47,28 @@ class App extends Component {
           <LambdaDemo />
         </header>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
 
+const veriff = Veriff({
+  host: "https://stationapi.veriff.com",
+  apiKey: "c11452b5-be99-4b52-8941-347ee3e8bc20",
+  parentId: "veriff-root",
+  onSession: function (err, response) {
+    window.veriffSDK.createVeriffFrame({ url: response.verification.url });
+  },
+});
+veriff.setParams({
+  vendorData: " ",
+});
+veriff.mount({
+  formLabel: {
+    givenName: "Prénom",
+    lastName: "Nom",
+  },
+  submitBtnText: "Test pour Åsa",
+  loadingText: "Chargement",
+});
